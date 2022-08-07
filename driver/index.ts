1c6f73f8170b205d77f402b6f27ba85e65570103
Nigel Lee <lee.nigel@hotmail.com>
2022-08-07 10:33:18 +0800

    feat: Driver boilerplate

---
+++ driver/index.ts
@@ -0,0 +1,31 @@
+import { parse } from "parser";
+import * as fs from "fs";
+
+function main() {
+	const args = process.argv;
+
+	if (args.length !== 3 || !args[2].includes(".js")) {
+		console.error("Usage: yarn [start|dev] <filename.js>");
+		process.exit(1);
+	}
+
+	const filename = args[2];
+
+	let SOURCE_CODE;
+	try {
+		SOURCE_CODE = fs.readFileSync(filename, { encoding: "utf-8" });
+	} catch (e) {
+		console.error("Unable to read file");
+	}
+
+	if (!SOURCE_CODE) {
+		console.error("NO SOURCE CODE");
+		process.exit(1);
+	}
+
+	const ast = parse(SOURCE_CODE);
+	
+	// evaluate using rust
+}
+
+main();
--- parser/index.ts
+++ parser/index.ts
@@ -1,1 +1,14 @@
-console.log(1);
+import { Node, Options, parse as acornParse } from "acorn";
+
+export function parse(sourceCode: string): Node {
+	const acornOptions: Options = {
+		ecmaVersion: "latest",
+	};
+
+	const ast: Node = acornParse(sourceCode, acornOptions);
+
+	// Change top level program to a block statement
+	ast.type = "BlockStatemenet";
+
+	return ast;
+}
