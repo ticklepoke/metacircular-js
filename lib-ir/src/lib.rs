c25621ec86fa86d137445cc66f61837467719dba
Nigel Lee <lee.nigel@hotmail.com>
2022-08-07 23:26:13 +0800

    feat: Wire up deserializer

--- lib-ir/src/lib.rs
+++ lib-ir/src/lib.rs
@@ -1,6 +1,8 @@
 mod ast;
 
 // Creates an estree compliant representation from a serialized ast string
-pub fn serialize(ast: String) {
-
+pub fn serialize(ast: String) -> ast::Node {
+    let parsed_ast: ast::Node =
+        serde_json::from_str(ast.as_str()).expect("Unable to de-serialize AST");
+    parsed_ast
 }
--- rs-evaluator/src/lib.rs
+++ rs-evaluator/src/lib.rs
@@ -1,7 +1,14 @@
+use lib_ir;
 use wasm_bindgen::prelude::*;
-
 
 #[wasm_bindgen]
 pub fn give(name: &str) -> String {
     name.to_string()
+}
+
+#[wasm_bindgen]
+pub fn evaluate(ast: String) {
+    let ast = lib_ir::serialize(ast);
+
+    // pass the ast to the evaluator to run
 }
