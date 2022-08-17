9c50a2a27fe7cca040af0dd6071c1676da5fb914
Nigel Lee <ticklepoke@users.noreply.github.com>
2022-08-17 20:05:18 +0800

    feat: Add js_sys, rename js_value

--- rs-evaluator/src/js_value.rs
+++ rs-evaluator/src/js_value.rs
@@ -1,6 +1,8 @@
 use lib_ir::ast::Literal;
 use wasm_bindgen::prelude::*;
 
+// TODO: we can evaluate more than just literals, objects would be returned
+// as their stringified representation
 pub fn map_rust_value(eval_result: Literal) -> JsValue {
     match eval_result.value {
         lib_ir::ast::LiteralValue::String(s) => JsValue::from(s),
@@ -9,7 +11,10 @@ pub fn map_rust_value(eval_result: Literal) -> JsValue {
             false => JsValue::FALSE,
         },
         lib_ir::ast::LiteralValue::Null => JsValue::NULL,
-        lib_ir::ast::LiteralValue::Number(n) => JsValue::from(n),
+        lib_ir::ast::LiteralValue::Number(n) => match n {
+            lib_ir::ast::JsNumber::Number(n) => JsValue::from(n),
+            lib_ir::ast::JsNumber::Nan => JsValue::from("NaN"), 
+        },
         lib_ir::ast::LiteralValue::RegExp => unreachable!(),
         lib_ir::ast::LiteralValue::Undefined => JsValue::UNDEFINED,
     }
